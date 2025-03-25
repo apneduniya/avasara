5a7a2bab6deb1fec42186d66fa344237ba9adf6e
thatsmeadarsh <thatsmeadarshgupta@gmail.com>
2025-03-25 14:31:33 +0530

    fix: typescript type issues

--- frontend/components/ui/animated-tooltip.tsx
+++ frontend/components/ui/animated-tooltip.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
+
 import Image from "next/image";
 import React, { useState } from "react";
 import {
